8741dc51441db4d2cd67e7505b29001ae8a7c250
AleCandido <candido.ale@gmail.com>
2019-11-15 19:59:53 +0100

    main module created, test added

---
+++ setup.py
@@ -0,0 +1,25 @@
+# Installation script for python
+from setuptools import setup, find_packages
+
+setup(name='dis',
+      version='0.0.1',
+      description='Deep Inelastic Scattering',
+      author = 'F. Hekhorn,  S.Carrazza, A.Candido',
+      author_email='stefano.carrazza@cern.ch',
+      url='https://github.com/N3PDF/dis',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      zip_safe=False,
+      classifiers=[
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Physics',
+      ],
+      install_requires=[
+          'numpy',
+          'numba',
+      ],
+      python_requires='>=3.6'
+)
