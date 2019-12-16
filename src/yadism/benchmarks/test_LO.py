36e54898012796f40cd181fb14f90c2ae60e3867
AleCandido <candido.ale@gmail.com>
2019-12-16 19:12:55 +0100

    test_LO cleaning

--- src/yadism/benchmarks/test_LO.py
+++ src/yadism/benchmarks/test_LO.py
@@ -12,98 +12,6 @@ from yadism.benchmarks.apfel_import import load_apfel
 def test_loader():
     """Test the loading mechanism"""
 
-    # Allocate a theory from NNPDF database at LO (theory.ID = 52)
-    theory52 = {
-        "ID": 52,
-        "PTO": 1,
-        "FNS": "FONLL-B",
-        "DAMP": 0,
-        "IC": 1,
-        "ModEv": "TRN",
-        "XIR": 1.0,
-        "XIF": 1.0,
-        "NfFF": 5,
-        "MaxNfAs": 5,
-        "MaxNfPdf": 5,
-        "Q0": 1.65,
-        "alphas": 0.118,
-        "Qref": 91.2,
-        "QED": 0,
-        "alphaqed": 0.007496252,
-        "Qedref": 1.777,
-        "SxRes": 0,
-        "SxOrd": "LL",
-        "HQ": "POLE",
-        "mc": 1.51,
-        "Qmc": 1.51,
-        "kcThr": 1.0,
-        "mb": 4.92,
-        "Qmb": 4.92,
-        "kbThr": 1.0,
-        "mt": 172.5,
-        "Qmt": 172.5,
-        "ktThr": 1.0,
-        "CKM": "0.97428 0.22530 0.003470 0.22520 0.97345 0.041000 0.00862 0.04030 0.999152",
-        "MZ": 91.1876,
-        "MW": 80.398,
-        "GF": 1.1663787e-05,
-        "SIN2TW": 0.23126,
-        "TMC": 1,
-        "MP": 0.938,
-        "Comments": "NNPDF3.1 NLO central",
-        "global_nx": 0,
-        "EScaleVar": 1.0,
-    }
-
-    # Allocate a theory from NNPDF database at LO (theory.ID = 22)
-    """theory = {
-        "ID": 22,
-        "PTO": 0,
-        "FNS": "ZM-VFNS",
-        "DAMP": 0,
-        "IC": 0,
-        "ModEv": "EXA",
-        "XIR": 1.0,
-        "XIF": 1.0,
-        "NfFF": 5,
-        "MaxNfAs": 5,
-        "MaxNfPdf": 5,
-        "Q0": 1.275,
-        "alphas": 0.11800000000000001,
-        "Qref": 91.2,
-        "QED": 0,
-        "alphaqed": 0.007496251999999999,
-        "Qedref": 1.777,
-        "SxRes": 0,
-        "SxOrd": "LL",
-        "HQ": "POLE",
-        "mc": 1.275,
-        "Qmc": 1.275,
-        "kcThr": 1.0,
-        "mb": 4.18,
-        "Qmb": 4.18,
-        "kbThr": 1.0,
-        "mt": 173.07,
-        "Qmt": 173.07,
-        "ktThr": 1.0,
-        "CKM": "0.97428 0.22530 0.003470 0.22520 0.97345 0.041000 0.00862 0.04030 0.999152",
-        "MZ": 91.1876,
-        "MW": 80.398,
-        "GF": 1.1663787e-05,
-        "SIN2TW": 0.23126,
-        "TMC": 0,
-        "MP": 0.938,
-        "Comments": "LO baseline for small-x res",
-        "global_nx": 0,
-        "EScaleVar": 1,
-    }
-
-    dis_observables = {
-        "F2": [{"x": 0.1, "Q2": 100}],
-        "xgrid": np.logspace(-3, 0, 25),
-        "is_log_interpolation": True,
-        "polynom_rank": 4,
-    }"""
     # read file
     with open("test_LO.yaml", "r") as file:
         test_dict = yaml.safe_load(file)
@@ -111,10 +19,7 @@ def test_loader():
     # execute DIS
     result = run_dis(test_dict)
 
-    # pprint(result)
-    # quit()
-
-    # setuo LHAPDF
+    # setup LHAPDF
     n31lo = lhapdf.mkPDF("NNPDF31_lo_as_0118", 0)
 
     def get_singlet(x, Q2, Nf):
@@ -124,7 +29,6 @@ def test_loader():
                     n31lo.xfxQ2(k, x, Q2) + n31lo.xfxQ2(-k, x, Q2)
                     for k in range(1, Nf + 1)
                 ]
-                + [n31lo.xfxQ2(0, x, Q2)]
             )
             / x
         )
