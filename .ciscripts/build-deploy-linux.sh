55cadd80da4d5ea15cc45dc8e0b320de510f7923
juacrumar <juacrumar@gmail.com>
2019-12-20 11:11:25 +0100

    add missing dep

--- .ciscripts/build-deploy-linux.sh
+++ .ciscripts/build-deploy-linux.sh
@@ -6,6 +6,14 @@ set -o pipefail
 set -u
 set -v
 
+
+# Let us assume at this point we are already inside conda
+# not sure about this, let Travis test
+git clone git@github.com:N3PDF/eko.git
+cd eko
+pip install .
+cd ..
+
 #Build package
 CONDA_PY=$CONDA_PY conda build -q conda-recipe
 if [ $? != 0 ]; then
