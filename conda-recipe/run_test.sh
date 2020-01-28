25ac5be8bdc72dbd431c9100a5ecd1906c8a4630
AleCandido <candido.ale@gmail.com>
2020-01-28 16:20:05 +0100

    Install manually eko at specific commit.

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -3,8 +3,15 @@ set -u
 set -v
 set -e
 
-#Python tests for the installed validphys package
-pytest --pyargs yadism
+# Try to install eko with pip
+ls -a
+cd eko
+git checkout 278cfa0d746b64fd5db6de85995d85b471f5d20a
+cd ..
+pip install eko/
 
-#Print linkage data
+# Python tests for the installed validphys package
+pytest --pyargs yadism --ignore=eko
+
+# Print linkage data
 conda inspect linkages -p $PREFIX $PKG_NAME
