322674b2b89613a0a8beeb200a1fa9ce56614f27
AleCandido <candido.ale@gmail.com>
2020-01-31 10:47:20 +0100

    Defined inspector structure.

--- src/yadism/input/inspector.py
+++ src/yadism/input/inspector.py
@@ -9,14 +9,84 @@
 #
 # make use of the values stored in available.yaml and default.yaml
 
+import os
+
+import yaml
+
+# ┌───────────────────┐
+# │ Arguments Classes │
+# └───────────────────┘
+
 
 class Argument:
+    """Short summary."""
+
     pass
 
 
 class EnumArgument(Argument):
+    """Short summary."""
+
     pass
 
 
 class RealArgument(Argument):
+    """Short summary."""
+
     pass
+
+
+# ┌───────────────────────────┐
+# │ Cross Constraints Classes │
+# └───────────────────────────┘
+
+
+class CrossConstraint:
+    pass
+
+
+# ╔═══════════╗
+# ║ Inspector ║
+# ╚═══════════╝
+
+
+class Inspector:
+    """Short summary.
+
+    """
+
+    def __init__():
+        """Short summary.
+
+        Returns
+        -------
+        type
+            Description of returned object.
+
+        """
+        self.input_dir = os.path.dirname(os.path.realpath(__file__))
+        pass
+
+    def check_domains():
+        """Short summary.
+
+        Returns
+        -------
+        type
+            Description of returned object.
+
+        """
+
+        domain_file = os.path.join(self.input_dir, "domain.yaml")
+        with open(domain_file, "r") as file:
+            domains = yaml.load(file)
+
+    def check_cross_constraints():
+        """Short summary.
+
+        Returns
+        -------
+        type
+            Description of returned object.
+
+        """
