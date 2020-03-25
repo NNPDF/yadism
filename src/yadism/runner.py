97d01a58504e9849c81e67de08b124b15f11c2e7
AleCandido <candido.ale@gmail.com>
2020-03-25 16:47:48 +0100

    Working with new interpolation

--- src/yadism/runner.py
+++ src/yadism/runner.py
@@ -6,25 +6,19 @@ import copy
 
 import numpy as np
 
-from eko import t_float
-from eko.interpolation import (
-    get_Lagrange_basis_functions,
-    get_Lagrange_basis_functions_log,
-)
+from eko.interpolation import InterpolatorDispatcher
 from yadism.structure_functions.LO import f2_light_LO_quark
 
 
 def get_configurations(observable, sq_charge_av):
-    def singlet(x: t_float, Q2: t_float, polynom_coeff: dict) -> t_float:
+    def singlet(x: float, Q2: float, polynom_f) -> float:
         pref_f2_singlet = x * sq_charge_av
-        return pref_f2_singlet * f2_light_LO_quark(
-            x, Q2, polynom_coeff, is_log_interpolation
-        )
+        return pref_f2_singlet * f2_light_LO_quark(x, Q2, polynom_f)
 
-    def gluon(x: t_float, Q2: t_float, polynom_coeff: dict) -> t_float:
+    def gluon(x: float, Q2: float, polynom_coeff: dict) -> float:
         return None
 
-    def nonsinglet(x: t_float, Q2: t_float, polynom_coeff: dict) -> t_float:
+    def nonsinglet(x: float, Q2: float, polynom_coeff: dict) -> float:
         return None
 
     return [("S", singlet), ("g", None), ("NS", None)]
@@ -51,8 +45,6 @@ def run_dis(theory: dict, dis_observables: dict) -> dict:
 
     # reading theory parameters
     n_f = theory["NfFF"]
-    if 6 < n_f <= 2:
-        raise ValueError("Number of flavors 'NfFF' must be in the range [3,6].")
 
     # compute charge factors
     charges = np.array([-1 / 3, 2 / 3] * 3)
@@ -62,20 +54,17 @@ def run_dis(theory: dict, dis_observables: dict) -> dict:
 
     # compute input grid
     is_log_interpolation = dis_observables.get("is_log_interpolation", True)
-    if is_log_interpolation:
-        get_fnc = get_Lagrange_basis_functions_log
-    else:
-        get_fnc = get_Lagrange_basis_functions
     xgrid = dis_observables["xgrid"]
-    coeffs = get_fnc(xgrid, dis_observables["polynom_rank"])
-    print("==========")
-    print(coeffs)
+    polynomial_degree = 2
+    interpolator = InterpolatorDispatcher(
+        xgrid, polynomial_degree, is_log_interpolation, mode_N=False
+    )
 
     # setup parameters
 
     # prepare the output
     output = {"xgrid": xgrid}
-    empty = np.zeros(len(coeffs))
+    empty = np.zeros(len(xgrid))
     output_vectors = dict(S=empty.copy(), NS=empty.copy(), g=empty.copy())
     for obs in ["F2", "FL"]:
         if obs in dis_observables:
@@ -88,7 +77,7 @@ def run_dis(theory: dict, dis_observables: dict) -> dict:
                 output[obs].append({**kinematics, **copy.deepcopy(output_vectors)})
 
     # iterate all polynomials
-    for c, coeff in enumerate(coeffs):
+    for c, poly_f in enumerate(interpolator):
         # iterate F2 configurations
         for k, kinematics in enumerate(dis_observables.get("F2", [])):
             # iterate contributions
@@ -96,9 +85,7 @@ def run_dis(theory: dict, dis_observables: dict) -> dict:
                 # skip zeros
                 if None == f:
                     continue
-                output["F2"][k][key][c] = f(
-                    kinematics["x"], kinematics["Q2"], coeff, is_log_interpolation
-                )
+                output["F2"][k][key][c] = f(kinematics["x"], kinematics["Q2"], poly_f)
 
     # TODO implement all other processes: FL, sigma, ?
     return output
--- src/yadism/structure_functions/__init__.py
+++ src/yadism/structure_functions/__init__.py
@@ -4,9 +4,3 @@ This subpackage contains the implementation of the DIS structure functions.
 
 The 3-loop reference is :cite:`Vermaseren:2005qc` which includes also the lower order results.
 """
-
-from eko import t_float
-from eko.interpolation import (
-    evaluate_Lagrange_basis_function_x,
-    evaluate_Lagrange_basis_function_log_x,
-)
