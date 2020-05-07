693c4d2bd3906342cb24b86a9a0df92571915f52
AleCandido <candido.ale@gmail.com>
2020-05-07 10:23:32 +0200

    Quick improve navigator dict comparison

--- benchmarks/navigator.py
+++ benchmarks/navigator.py
@@ -511,7 +511,7 @@ def subtract_tables(id1, id2):
 diff = subtract_tables
 
 
-def compare_dicts(d1, d2):
+def compare_dicts(d1, d2, exclude_underscored=False):
     """
         Check which entries of the two dictionaries are different, and output
         the values.
@@ -520,6 +520,9 @@ def compare_dicts(d1, d2):
     fw = 30  # values print width0
     print("┌", "─" * (kw + 2), "┬", "─" * (fw * 2 + 1 + 2), "┐", sep="")
     for k in d1.keys() | d2.keys():
+        if exclude_underscored and k[0] == "_":
+            continue
+
         if k not in d1:
             print(f"│ {k:<{kw}} │ {None:>{fw}} {d2[k]:>{fw}} │")
         elif k not in d2:
