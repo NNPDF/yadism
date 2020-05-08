c8e0a47517267850b10594cc97bdb617b375c284
AleCandido <candido.ale@gmail.com>
2020-05-08 14:40:16 +0200

    Verbose table

--- benchmarks/data/theory1.py
+++ benchmarks/data/theory1.py
@@ -3,6 +3,11 @@ import tinydb
 
 db = tinydb.TinyDB("input.json")
 theories_table = db.table("theories")
+
+theories_table.insert({1: 2, 3: 4})
+print(theories_table)
+print(dir(theories_table), flush=True)
+
 theories_table.purge()
 
 theories_table.insert({1: 2, 3: 4})
