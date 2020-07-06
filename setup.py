417b94f596ab6b2b4a2348e0bc6fb4fee794f429
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2020-07-06 12:43:50 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yadism",
-    version="0.3.0",
+    version="0.3.2",
     description="Deep Inelastic Scattering",
     author="A.Candido, S.Carrazza, F. Hekhorn",
     author_email="stefano.carrazza@cern.ch",
