d53796fed0660e7f8e003c7520f6303b0aa3af92
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2020-07-22 10:47:41 +0200

    Adjust version number

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -27,7 +27,7 @@ author = "N3PDF team"
 # The short X.Y version
 version = "0.3"
 # The full version, including alpha/beta/rc tags
-release = "0.3.2"
+release = "0.3.3"
 
 # -- General configuration ---------------------------------------------------
 
