cef2aef9f521e51b9ac4ab6e47a479c30b35c0f8
AleCandido <candido.ale@gmail.com>
2020-09-16 10:28:09 +0200

    Fix broken references in sphinx docs, provide global docs Makefile and demo

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -105,7 +105,7 @@ rst_prolog = "\n".join([open(x).read() for x in os.scandir(shared)])
 #
 html_theme = "sphinx_rtd_theme"
 
-html_logo = "../../logo/logo-docs.png"
+html_logo = "../../_assets/logo/logo-docs.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -148,7 +148,7 @@ html_context = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
