0aeed05da49f6744af30203b4cb502106317f423
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2020-10-16 14:27:28 +0200

    activate APFEL assert again

--- benchmarks/runners/benchmark_against_apfel.py
+++ benchmarks/runners/benchmark_against_apfel.py
@@ -16,7 +16,7 @@ class ApfelBenchmark:
 
     def _db(self, assert_external=None):
         """init DB connection"""
-        assert_external = False
+        #assert_external = False
         self.db = DBInterface("APFEL", assert_external=assert_external)
         return self.db
 
