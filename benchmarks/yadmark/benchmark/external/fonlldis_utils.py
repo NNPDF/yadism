68a0570a83d94424b469b97c4b079d3e38c4685d
Giacomo Magni <giacomomagni@Giacomos-MacBook-Pro.local>
2020-12-07 09:04:41 +0100

    working on FONLLdis benchmark

--- benchmarks/yadmark/benchmark/external/fonlldis_utils.py
+++ benchmarks/yadmark/benchmark/external/fonlldis_utils.py
@@ -2,6 +2,7 @@ import pathlib
 import numpy as np
 import yaml
 import math
+import os 
 
 from yadism import observable_name as on
 
@@ -35,7 +36,7 @@ def compute_FONLLdis_data( theory, observables, pdf):
 
     if scheme.endswith('A'): 
         scheme='A'
-        iord=theory["PTO"] 
+        iord=theory["PTO"]
         iford=theory["PTO"]-1
         if iford < 0: raise Exception("FONLL-A at LO can not be computed!")
     elif scheme.endswith('B'): 
@@ -53,8 +54,8 @@ def compute_FONLLdis_data( theory, observables, pdf):
     mb2 = theory["mb"] ** 2
     mt2 = theory["mt"] ** 2
     mc = theory["mc"]
-    alphas = theory[alphas]
-    q2ref = theory[Qref]
+    alphas = theory["alphas"]
+    q2ref = theory["Qref"]
 
     # singlet + gluon as default else select gluon only 
     isin = 1
@@ -62,19 +63,19 @@ def compute_FONLLdis_data( theory, observables, pdf):
 
 
     # TODO:
-    # 1) check if this initialization is working 
-    # 2) check for the other grids ... 
+    # 1) check if this initialization is working for toyLH
 
     # Init PDF
     iwhichpdf = 1.
-    if pdf.name == "ToyLH":
+    #if pdf.name == "ToyLH":
+    if pdf == "ToyLH":
         iwhichpdf = 0.
-        LHAPDFfile = 'toyLH_NLO.grid'
-        FONLLdis.initnnpdfwrap(LHAPDFfile,NREP)
+        LHAPDFfile = "toyLH_NLO.grid"
+        FONLLdis.initnnpdfwrap(LHAPDFfile)
 
     else:
         # Init LHAPDF 
-        FONLLdis.mkpdf( pdf.name, 0 )
+        FONLLdis.mkpdf( pdf, 0 )
 
     num_tab={}
     # loop over functions 
@@ -85,10 +86,11 @@ def compute_FONLLdis_data( theory, observables, pdf):
 
         obs_name = on.ObservableName(obs)
 
-        if obs_name.flavor != "total" or obs_name.flavor != "charm":
+        if obs_name.flavor != "total" and obs_name.flavor != "charm":
             raise NotImplementedError(f"{obs_name.flavor} is not implemented in FONLLdis")          
 
-        # loop over points 
+        out = []
+        # loop over points
         for kin in observables[obs]:
 
             x = kin["x"]
@@ -98,47 +100,50 @@ def compute_FONLLdis_data( theory, observables, pdf):
             fcmm = 0.
             fcm0 = 0.
 
-            # Init QCDNUM 
-            FONLLdis.zmstf(mc2, mb2, mt2, alphas, q2ref, q2, iord, iwhichpdf)
-
-            # TODO: is asfunc imported correctly ?? 
-            # Get alphas 
-            alphaq2 = FONLLdis.asfunc(q2)
-
-            # Massive part 
-            fcmm = FONLLdis.f2massive( x, q2, mc,\
-                alphaq2, iord, iford, obs_name.kind," ",isin) 
-            fcm0 = FONLLdis.f2massive( x, q2, mc,\
-                alphaq2, iord, iford, obs_name.kind,"a",isin) 
-
-
-            # Massless structure functions
-            if obs_name.kind == "FL":
-
-                fczm = FONLLdis.fflczm( x, q2 )
-                # compute light if total
-                if obs_name.flavor == "total":
-                    flzm = FONLLdis.ffllzm( x, q2 )
-                
-            elif obs_name.kind == "F2":
-
-                fczm = FONLLdis.ff2czm( x, q2 )
-                # compute light if total 
-                if obs_name.flavor == "total":    
-                    flzm = FONLLdis.ff2lzm( x, q2 )
+            if x != 1.0:
             
-            else:
-                raise NotImplementedError(f"kind {obs_name.name} is not implemented!")
+                # Init QCDNUM 
+                FONLLdis.zmstf(mc2, mb2, mt2, alphas, q2ref, q2, iord, iwhichpdf)
+
+                # TODO: is asfunc imported correctly ?? 
+                # Get alphas 
+                alphaq2 = FONLLdis.asfunc(q2)
+
+                # Massive part 
+                fcmm = FONLLdis.f2massive( x, q2, mc,\
+                    alphaq2, iord, iford, obs_name.kind," ",isin) 
+                fcm0 = FONLLdis.f2massive( x, q2, mc,\
+                    alphaq2, iord, iford, obs_name.kind,"a",isin)
+
+            
+                # Massless structure functions
+                if obs_name.kind == "FL":
+
+                    fczm = FONLLdis.fflczm( x, q2 )
+                    # compute light if total
+                    if obs_name.flavor == "total":
+                        flzm = FONLLdis.ffllzm( x, q2 )
+                
+                elif obs_name.kind == "F2":
+
+                    fczm = FONLLdis.ff2czm( x, q2 )
+                    # compute light if total 
+                    if obs_name.flavor == "total":    
+                        flzm = FONLLdis.ff2lzm( x, q2 )
+            
+                else:
+                    raise NotImplementedError(f"kind {obs_name.name} is not implemented!")
 
             # Plain FONLL
-            ffonll = ( fczm - fcm0 + f2cmm ) + flzm 
+            print( fczm, flzm, fcmm, fcm0)
+            ffonll = ( fczm - fcm0 + fcmm ) + flzm 
             
             # FONLL with threshold damping
             threshold = 0.
-            if math.sqrt( q2) >= mc:
+            if math.sqrt( q2 ) >= mc:
                 threshold =  ( 1. - mc ** 2 / q2 ) ** 2 
 
-            ffonll_thres = ( fczm - fcm0 ) * threshold + fcmm 
+            ffonll_thres = ( fczm - fcm0 ) * threshold + fcmm + flzm
 
             #output tab  
             out.append(dict(x=x, Q2=q2, value=ffonll, value_thres=ffonll_thres))
@@ -147,8 +152,23 @@ def compute_FONLLdis_data( theory, observables, pdf):
 
 
     # TODO: do we need this here? 
-    # remove QCDNUM cache files
-    for f in [wname, zmname ]:
-        pathlib.Path(f).unlink(missing_ok=True)
+    os.system("rm -rf *.wgt")
 
     return num_tab
+
+
+
+if __name__ == "__main__":
+    
+
+    with open("theory.yaml") as f:
+        theory = yaml.safe_load(f)
+
+    with open("obs.yaml") as f:
+        obs = yaml.safe_load(f)
+
+
+    #print( compute_FONLLdis_data( theory, obs, "ToyLH" ) )
+    print( compute_FONLLdis_data( theory, obs, "CT14llo_NF4" ) )
+
+
