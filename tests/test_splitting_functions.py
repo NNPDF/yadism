39321b3462cb1312e378cec22af9d49a36d438f0
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2021-01-07 17:50:39 +0100

    Fix test splitting functions

--- tests/test_splitting_functions.py
+++ tests/test_splitting_functions.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from unittest import mock
 import pytest
 
 import yadism.splitting_functions as split
 
 
-# class TestSplittingLO:
-#     def test_pqq(self):
-#         assert split.pqq_reg(-1, MockConstants()) == 0
-#         assert split.pqq_reg(0.35, MockConstants(CF=0)) == 0
+class TestSplittingLO:
+    def test_pqq(self):
+        assert split.pqq_reg(-1) == 0
+        with mock.patch("eko.constants.CF", 0):
+            assert split.pqq_reg(0.35) == 0
+            assert split.pqq_sing(0.35) == 0
+            assert split.pqq_local(0.35) == 0
 
-#     def test_pqg(self):
-#         assert split.pqg(0.61, MockConstants(TF=0)) == 0
+    def test_pqg(self):
+        with mock.patch("eko.constants.TR", 0):
+            assert split.pqg(0.61) == 0
