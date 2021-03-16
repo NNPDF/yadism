d35e4f9cf89d87c0eec5cae1c6c8c2dbde8bdf94
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2021-03-16 18:19:26 +0100

    Block IC CC

--- benchmarks/runners/apfel_ic_bench.py
+++ benchmarks/runners/apfel_ic_bench.py
@@ -13,29 +13,32 @@ class ApfelICBenchmark(Runner):
 
     external = "APFEL"
 
-    def obs_updates(self):
+    def obs_updates(self, allow_cc=False):
         kinematics = []
         kinematics.extend([dict(x=x, Q2=10.0) for x in np.geomspace(1e-5, 1, 10)])
         kinematics.extend(
             [dict(x=0.1, Q2=Q2) for Q2 in np.geomspace(4, 20, 10).tolist()]
         )
-        return [
-            {
-                "prDIS": "CC",
-                "observables": {
-                    f: kinematics for f in ["F2charm", "FLcharm", "F3charm"]
-                },
-            },
+        obs = [
             {
                 "prDIS": "NC",
                 "observables": {f: kinematics for f in ["F2charm", "FLcharm"]},
             },
         ]
+        obs.append(
+            {
+                "prDIS": "CC",
+                "observables": {
+                    f: kinematics for f in ["F2charm", "FLcharm", "F3charm"]
+                },
+            }
+        )
+        return obs
 
 
 class BenchmarkFFNS(ApfelICBenchmark):
     def benchmark_lo(self):
-        self.run([{"IC": 1}], self.obs_updates(), ["conly", "CT14llo_NF4"])
+        self.run([{"IC": 1}], self.obs_updates(True), ["conly", "CT14llo_NF4"])
 
     def benchmark_nlo(self):
         self.run([{"PTO": 1, "IC": 1}], self.obs_updates(), ["conly", "CT14llo_NF4"])
@@ -53,5 +56,6 @@ class BenchmarkFONLL(ApfelICBenchmark):
 if __name__ == "__main__":
     ffns = BenchmarkFFNS()
     ffns.benchmark_lo()
-    # plain.benchmark_nlo()
+    ffns.benchmark_nlo()
     fonll = BenchmarkFONLL()
+    fonll.benchmark_nlo()
