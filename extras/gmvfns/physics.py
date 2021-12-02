b488c51cac214901791348b196a55dcdd11a53bd
Alessandro Candido <candido.ale@gmail.com>
2021-12-02 11:53:37 +0100

    Split combiner into components

--- extras/gmvfns/physics.py
+++ extras/gmvfns/physics.py
@@ -1,49 +1,64 @@
 from .elements import VFNS, Component, KernelGroup, Patch
 
+def light_component(nf, masses):
+    comp = Component(0)
+    # the first condition essentially checks nf != 3
+    if nf in masses and masses[nf]:
+        comp.append(KernelGroup("light", nf, ihq=nf, fonll=True))
+    else:
+        comp.append(KernelGroup("light", nf))
+
+    for ihq in range(nf + 1, 7):
+        if masses[ihq]:
+            comp.append(KernelGroup("miss", nf, ihq=ihq, nc=nf))
+    return comp
+
+def heavylight_components(nf, hq, masses):
+    comps = []
+    if hq < nf or (hq == nf and not masses[hq]):
+        heavylight = Component(hq)
+        heavylight.append(KernelGroup("light", nf, ihq=hq, nc=1))
+        comps.append(heavylight)
+
+    return comps
+
+def heavy_components(nf, hq, masses):
+    comps = []
+    heavy = {}
+    for sfh in range(nf, 7):
+        # if it's ZM you don't even have the component
+        # exclude sfh=3, since heavy contributions are there for [4,5,6]
+        if sfh in masses and masses[sfh]:
+            heavy[sfh] = Component(sfh)
+            if hq != 0 and hq != sfh:
+                continue
+
+            if sfh == nf:
+                heavy[sfh].append(KernelGroup("heavy", nf, ihq=sfh, fonll=True))
+            else:
+                heavy[sfh].append(KernelGroup("heavy", nf, ihq=sfh))
+
+            for ihq in range(sfh + 1, 7):
+                if masses[ihq]:
+                    heavy[sfh].append(KernelGroup("miss", nf, ihq=ihq, nc=1))
+                comps.append(heavy[sfh])
+
+    return comps
+
 
 def combiner(obs, nf, masses):
     p = Patch(obs.name, nf)
 
     # Adding light component
     if obs.family in ["light", "total"]:
-        light = Component(0)
-        # the first condition essentially checks nf != 3
-        if nf in masses and masses[nf]:
-            light.append(KernelGroup("light", nf, ihq=nf, fonll=True))
-        else:
-            light.append(KernelGroup("light", nf))
-
-        for ihq in range(nf + 1, 7):
-            if masses[ihq]:
-                light.append(KernelGroup("miss", nf, ihq=ihq, nc=nf))
-
-        p.append(light)
+        p.append(light_component(nf, masses))
     if obs.family == "heavy":
         #  the only case in which an heavy contribution is not present in those
         #  accounted for in total, it's whene heavy already became heavylight
-        if obs.hq < nf or (obs.hq == nf and not masses[obs.hq]):
-            heavylight = Component(obs.hq)
-            heavylight.append(KernelGroup("light", nf, ihq=obs.hq, nc=1))
-            p.append(heavylight)
+        p.extend(heavylight_components(nf, obs.hq, masses))
     if obs.family in ["heavy", "total"]:
-        heavy = {}
-        for sfh in range(nf, 7):
-            # if it's ZM you don't even have the component
-            # exclude sfh=3, since heavy contributions are there for [4,5,6]
-            if sfh in masses and masses[sfh]:
-                heavy[sfh] = Component(sfh)
-                if obs.hq != 0 and obs.hq != sfh:
-                    continue
+        p.extend(heavy_components(nf, obs.hq, masses))
 
-                if sfh == nf:
-                    heavy[sfh].append(KernelGroup("heavy", nf, ihq=sfh, fonll=True))
-                else:
-                    heavy[sfh].append(KernelGroup("heavy", nf, ihq=sfh))
-
-                for ihq in range(sfh + 1, 7):
-                    if masses[ihq]:
-                        heavy[sfh].append(KernelGroup("miss", nf, ihq=ihq, nc=1))
-                p.append(heavy[sfh])
     return p
 
 
