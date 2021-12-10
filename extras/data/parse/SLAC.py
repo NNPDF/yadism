49e528d425e42cd478f806e91d3d67ab94f24edb
Alessandro Candido <candido.ale@gmail.com>
2021-12-10 11:58:24 +0100

    Add SLAC observable generation

---
+++ extras/data/parse/SLAC.py
@@ -0,0 +1,30 @@
+from .utils import load, obs_template
+
+
+def dump(src_path):
+    """
+    Compute SLAC observables.
+
+    Parameters
+    ----------
+        src_path : str
+            input path
+        target_path : str
+            target path
+    """
+    obs = obs_template.copy()
+    data = load(src_path, 0, ["x", "Q2"])
+    esf = [dict(x=d["x"], y=1.0, Q2=d["Q2"]) for d in data]
+    obs["prDIS"] = "NC"
+    obs["observables"] = {"F2": esf}
+    obs["ProjectileDIS"] = "electron"
+    obs["TargetDIS"] = "proton"
+
+    return obs
+
+
+# renaming
+new_names = {
+    "slac_d": "SLAC_NC_EM_D_F2",
+    "slac_p": "SLAC_NC_EM_P_F2",
+}
