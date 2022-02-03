637e41dbb5b8a7864d49a1fab42204d03041450f
Alessandro Candido <candido.ale@gmail.com>
2022-02-03 01:09:42 +0100

    Add theory and makefile benchmark

--- extras/data/machinery/benchmark/__init__.py
+++ extras/data/machinery/benchmark/__init__.py
@@ -1,13 +1,44 @@
 import pathlib
 
+import yaml
+
+here = pathlib.Path(__file__).parent
+
 
 def cli(subparsers):
     ap = subparsers.add_parser(
         "benchmark", description="benchmark runcards in specified folders"
     )
     ap.add_argument("folders", nargs="+", type=pathlib.Path)
+    ap.add_argument(
+        "-t", "--theory", type=pathlib.Path, default=here / "theory_200_1.yaml"
+    )
     ap.set_defaults(func=main)
 
 
+def read_metadata(body):
+    metadata = {}
+
+    for line in body.splitlines():
+        terms = line.split("=")
+        metadata[terms[0]] = "=".join(terms[1:])
+
+    return metadata
+
+
+def benchmark(theory, observables):
+    pass
+
+
 def main(args):
-    print(args)
+    theory = yaml.safe_load((args.theory).read_text(encoding="utf-8"))
+    print("theory", theory["ID"], end="\n\n")
+
+    for folder in args.folders:
+        metadata = read_metadata((folder / "metadata.txt").read_text(encoding="utf-8"))
+        print(metadata["fktable_id"], metadata["arxiv"], sep=" - ")
+        observables = yaml.safe_load(
+            (folder / "observable.yaml").read_text(encoding="utf-8")
+        )
+        print("  target:", observables["TargetDIS"])
+        benchmark(theory, observables)
