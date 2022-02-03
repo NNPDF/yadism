e04c3d80726314451c2c967f5d6326c5c78138af
Alessandro Candido <candido.ale@gmail.com>
2022-02-03 11:58:36 +0100

    Scope lo comparison in a suitable option

--- extras/data/machinery/benchmark/__init__.py
+++ extras/data/machinery/benchmark/__init__.py
@@ -16,6 +16,7 @@ def cli(subparsers):
     ap.add_argument(
         "-t", "--theory", type=pathlib.Path, default=here / "theory_200_1.yaml"
     )
+    ap.add_argument("-lo", "--leading-order", action="store_true", dest="lo")
     ap.set_defaults(func=main)
 
 
@@ -32,8 +33,10 @@ def read_metadata(body):
 def main(args):
     theory = yaml.safe_load((args.theory).read_text(encoding="utf-8"))
     print("theory", theory["ID"], end="\n\n")
-    theory["PTO"] = 0
-    theory["FNS"] = "ZM-VFNS"
+    if args.lo:
+        print("LO comparison")
+        theory["PTO"] = 0
+        theory["FNS"] = "ZM-VFNS"
 
     for folder in args.folders:
         metadata = read_metadata((folder / "metadata.txt").read_text(encoding="utf-8"))
