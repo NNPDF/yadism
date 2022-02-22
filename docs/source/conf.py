da5f9d79fdb930c8f39f1ad618da86a7cec644c8
Alessandro Candido <candido.ale@gmail.com>
2022-02-22 11:16:40 +0100

    Add pandas intersphinx ref

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -302,6 +302,7 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "scipy": ("https://docs.scipy.org/doc/scipy", None),
     "numpy": ("https://numpy.org/doc/stable", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
 }
 
 
