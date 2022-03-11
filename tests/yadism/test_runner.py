3adf2823f252f502f597e8f63e4d76f61133cab2
Alessandro Candido <candido.ale@gmail.com>
2022-03-11 23:46:28 +0100

    Test simple runner methods

--- tests/yadism/test_runner.py
+++ tests/yadism/test_runner.py
@@ -1,14 +1,55 @@
 # -*- coding: utf-8 -*-
-from hypothesis import given
+import collections
+
+import pytest
+from hypothesis import HealthCheck, given, settings
 from hypothesis import strategies as st
 
-from yadism import runner
+from yadism import runner, sf
+
+
+class DropRunner(runner.Runner):
+    def __init__(self):
+        class DropSF(sf.StructureFunction):
+            def __init__(self):
+                self.passed = False
+
+            def drop_cache(self):
+                self.passed = True
+
+        class DropXS:
+            def __init__(self):
+                self.passed = True
+
+        self.observables = dict(a=DropSF(), b=DropXS(), c=DropSF(), d=DropXS())
+
+
+class GetRunner(runner.Runner):
+    def __init__(self):
+        self.observables = dict(a=None)
 
 
 class TestRunner:
     def test_init(self):
-        #  runner.Runner(theory=theory, observables=observables)
+        #  runner.Runner({}, {})
         pass
+
+    @given(st.text(min_size=2))
+    @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+    def test_get_sf(self, monkeypatch, name):
+        FakeName = collections.namedtuple("FakeName", ["name"])
+        FakeSF = collections.namedtuple("FakeSF", ["obsname", "runner"])
+        get_runner = GetRunner()
+
+        assert get_runner.get_sf(FakeName("a")) is None
+        monkeypatch.setattr(runner, "SF", FakeSF)
+        assert get_runner.get_sf(FakeName(name)).obsname.name == name
+
+    def test_drop(self):
+        drop_runner = DropRunner()
+
+        drop_runner.drop_cache()
+        assert all(o.passed for o in drop_runner.observables.values())
 
 
 class TestConfig:
@@ -22,19 +63,23 @@ class TestConfig:
     @given(
         st.dictionaries(st.text(), st.floats()),
         st.dictionaries(st.text(), st.functions()),
-        st.integers(),
+        st.data(),
     )
-    def test_getattribute(self, theory, managers, value):
-        managers["mykey"] = value
-        managers["yourkey"] = value
+    def test_getattribute(self, theory, managers, data):
         theory["mykey"] = None
+        theory["anotherkey"] = data.draw(st.binary())
+        managers["mykey"] = data.draw(st.integers())
+        managers["yourkey"] = data.draw(st.characters())
         config = runner.RunnerConfigs(theory, managers)
 
-        assert config.mykey == value
-        assert config.yourkey == value
+        assert config.mykey == managers["mykey"]
+        assert config.yourkey == managers["yourkey"]
+        assert config.anotherkey == theory["anotherkey"]
         assert isinstance(config.theory, dict)
 
-        managers["theory"] = value
+        managers["theory"] = data.draw(st.text())
         config = runner.RunnerConfigs(theory, managers)
 
-        assert config.theory == value
+        assert config.theory == managers["theory"]
+        with pytest.raises(AttributeError):
+            config.missing
