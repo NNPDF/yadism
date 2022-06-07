f3b670271598aeefcf792b14cf1d6cde00d3e95c
Alessandro Candido <candido.ale@gmail.com>
2022-06-07 12:23:59 +0200

    Test ESFInfo

---
+++ tests/yadism/esf/test_esf.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from yadism.esf.esf import ESFInfo
+
+
+class TestESFInfo:
+    def test_init(self):
+        obsn = "ciao"
+        confs = "come"
+        ESFInfo(obsn, confs)
+
+    def test_getattribute(self):
+        class MyObj:
+            def __init__(self, come=""):
+                self.come = come
+
+        obsn = "ciao"
+        confs = MyObj()
+        confs.come = "va"
+        e = ESFInfo(obsn, confs)
+
+        assert e.obs_name == obsn
+        assert e.configs == confs
+        assert e.come == confs.come
