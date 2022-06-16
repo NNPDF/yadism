5a835103f019035c1528816102449d8a87836e8c
Alessandro Candido <candido.ale@gmail.com>
2022-06-16 18:38:29 +0200

    Add CLI actions

--- extras/nnlo-cc-heavy/reformat/__init__.py
+++ extras/nnlo-cc-heavy/reformat/__init__.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 import argparse
+import logging
 import pathlib
 
+from . import generate as gen
 from . import parse
+
+_logger = logging.getLogger(__name__)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("path", type=pathlib.Path)
+    parser.add_argument("paths", nargs="*", type=pathlib.Path)
+    parser.add_argument("-a", "--actions", nargs="*", choices=["dump", "plot"])
 
     return parser.parse_args()
 
@@ -16,4 +21,20 @@ def parse_args():
 def main():
     args = parse_args()
 
-    parse.parse(args.path)
+    for path in args.paths:
+        ch, blocks = parse.parse(path)
+        ds = gen.to_xarray(blocks)
+
+        if "dump" in args.actions:
+            dumpdest = pathlib.Path.cwd() / "data" / f"{path.stem}.nc"
+            dumpdest.parent.mkdir(exist_ok=True)
+            ds.to_netcdf(dumpdest)
+
+            _logger.info(
+                f"Saved xgrid array to {dumpdest.relative_to(pathlib.Path.cwd())}"
+            )
+
+        if "plot" in args.actions:
+            dest = pathlib.Path.cwd() / "plots" / f"{path.stem}.png"
+            dest.parent.mkdir(exist_ok=True)
+            gen.plot(ds, dest)
