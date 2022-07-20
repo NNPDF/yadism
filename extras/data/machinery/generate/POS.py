3d41a76f134add3bb5a8befd260e4f8b396edf5c
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-07-20 16:50:53 +0200

    Add kinematics function

--- extras/data/machinery/generate/POS.py
+++ extras/data/machinery/generate/POS.py
@@ -4,6 +4,31 @@ import yaml
 from .utils import obs_template
 
 
+def _make_kins():
+    """Generate kinematics.
+
+    Routine is taken from https://github.com/NNPDF/nnpdf/blob/17952b40f02d23fc2b29a702ea93db6dc4e3b2fa/buildmaster/filters/POS.cc
+    and meta data from https://github.com/NNPDF/nnpdf/blob/17952b40f02d23fc2b29a702ea93db6dc4e3b2fa/buildmaster/meta/POSF2U.yaml .
+    """
+    # from meta
+    fNData = 20
+
+    q2pos = 5  # GeV2
+    xmin = 5e-7
+    xmax = 0.9
+    xch = 0.1
+    nxposlog = fNData / 2
+    step = (xmax - xch) / (fNData - nxposlog)
+
+    xs = []
+    for j in range(fNData):
+        if j < nxposlog:
+            xs.append(xmin * pow(xch / xmin, j / (nxposlog - 1)))
+        else:
+            xs.append(xch + step * (1 + j - nxposlog))
+    return [{"Q2": q2pos, "x": x, "y": 1.0} for x in xs]
+
+
 def dump(src_path, _target):
     """Compute positivity observables.
 
@@ -21,7 +46,8 @@ def dump(src_path, _target):
     obs = obs_template.copy()
     with open(src_path, "r", encoding="utf-8") as f:
         obs_update = yaml.safe_load(f)
-    obs.update(obs_update)
+    obs["NCPositivityCharge"] = obs_update["NCPositivityCharge"]
+    obs["observables"][obs_update["obs"]] = _make_kins()
     obs["prDIS"] = "EM"
     obs["ProjectileDIS"] = "electron"
     obs["TargetDIS"] = "proton"
