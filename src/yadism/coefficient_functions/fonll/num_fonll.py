77cc6a7f4f14b0514c7a2bf9797aa58bf196a572
andreab1997 <andreab1997@yahoo.it>
2023-02-27 11:43:59 +0100

    Add just the numerical fonll file

---
+++ src/yadism/coefficient_functions/fonll/num_fonll.py
@@ -0,0 +1,5 @@
+"""Implementation of numerical FONLL.
+
+The FONLL prescription can be also applied at observable level, i.e. subtracting 
+structure functions computed with different number of active flavors and PDFs. 
+"""
