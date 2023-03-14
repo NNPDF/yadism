9d18b0baa2b39aa5de12904952f94207a4497736
RoyStegeman <roystegeman@live.nl>
2023-03-14 11:20:50 +0000

    add g4 nnlo by importing f2 coefficients

--- src/yadism/coefficient_functions/light/g4_nc.py
+++ src/yadism/coefficient_functions/light/g4_nc.py
@@ -1,5 +1,5 @@
 from ..partonic_channel import RSL
-from . import nlo
+from . import nlo, nnlo
 from . import partonic_channel as pc
 
 
@@ -14,11 +14,14 @@ class NonSinglet(pc.LightBase):
     def NLO():
         """|ref| Eq. (19) of :cite:`Borsa:2022irn`."""
         return RSL.from_distr_coeffs(
-            nlo.g4.ns_reg, (nlo.g4.ns_delta, nlo.g4.ns_omx, nlo.g4.ns_logomx)
+            nlo.f2.ns_reg, (nlo.f2.ns_delta, nlo.f2.ns_omx, nlo.f2.ns_logomx)
         )
 
     def NNLO(self):
-        return None
+        """|ref| Eq. (19) of :cite:`Borsa:2022irn`."""
+        return RSL(
+            nnlo.xc2ns2p.c2nn2a, nnlo.xc2ns2p.c2ns2b, nnlo.xc2ns2p.c2nn2c, [self.nf]
+        )
 
 
 class Gluon(pc.LightBase):
--- src/yadism/coefficient_functions/light/nlo/__init__.py
+++ src/yadism/coefficient_functions/light/nlo/__init__.py
@@ -1,1 +1,1 @@
-from . import f2, f3, fl, g1, gl, g4
+from . import f2, f3, fl, g1
