92e8cbff9f0f24b245bb742c0236fc73f1e6b189
RoyStegeman <roystegeman@live.nl>
2023-03-14 11:06:17 +0000

    update reference for g4 ns coeffs

--- src/yadism/coefficient_functions/light/g4_nc.py
+++ src/yadism/coefficient_functions/light/g4_nc.py
@@ -6,19 +6,13 @@ from . import partonic_channel as pc
 class NonSinglet(pc.LightBase):
     @staticmethod
     def LO():
-        """
-        |ref| APFEL
-        """
-
+        """|ref| Eq. (4) of :cite:`Borsa:2022irn`."""
         # leading order is just a delta function
         return RSL.from_delta(1.0)
 
     @staticmethod
     def NLO():
-        """
-        |ref| hep-ph/9412255
-        """
-
+        """|ref| Eq. (19) of :cite:`Borsa:2022irn`."""
         return RSL.from_distr_coeffs(
             nlo.g4.ns_reg, (nlo.g4.ns_delta, nlo.g4.ns_omx, nlo.g4.ns_logomx)
         )
@@ -29,18 +23,7 @@ class NonSinglet(pc.LightBase):
 
 class Gluon(pc.LightBase):
     def NLO(self):
-        r"""
-        |ref| APFEL
-
-        Note
-        ----
-        2 * n_f is coming from momentum sum rule q_i -> {q_i, g} but
-        g -> {g, q_i, \bar{q_i} forall i}, so the 2 * n_f is needed
-        to compensate for all the number of flavours plus antiflavours
-        in which the gluon can go.
-        """
-
-        return RSL(nlo.g1.gluon_reg, args=[self.nf])
+        return None
 
     def NNLO(self):
         return None
