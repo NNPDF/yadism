6068446ece33897ca72f7d7db316ef5ec7a5f67a
andreab1997 <andreab1997@yahoo.it>
2023-03-22 11:53:23 +0100

    Fix N3LO

--- src/yadism/esf/scale_variations.py
+++ src/yadism/esf/scale_variations.py
@@ -129,8 +129,8 @@ class ScaleVariations:
         beta0 = beta.beta_qcd_as2(nf)
         ren_coeffs = {
             (2, 1, 1): + beta0,
-            (3, 1, 2): - 2 *beta0,
-            (3, 1, 1): - beta.beta_qcd_as3(nf),
+            (3, 1, 2): + 2 *beta0,
+            (3, 1, 1): + beta.beta_qcd_as3(nf),
             (3, 2, 1): + beta0 ** 2
         }
         return dict(filter(lambda item: item[0][0] <= self.order, ren_coeffs.items()))
