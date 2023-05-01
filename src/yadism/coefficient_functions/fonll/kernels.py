9b6ba4dc6894080f67f7ce9d4b10f8268bcecea6
Radonirinaunimi <rrabeman@nikhef.nl>
2023-05-02 00:02:11 +0200

    Just made a change to change base

--- src/yadism/coefficient_functions/fonll/kernels.py
+++ src/yadism/coefficient_functions/fonll/kernels.py
@@ -3,6 +3,8 @@
 This is strictly following the original reference :cite:`forte-fonll`, and
 implements the prescription at coefficient functions level.
 
+# TODO: Implement Polarized FONLL
+
 """
 import numpy as np
 from eko import basis_rotation as br
