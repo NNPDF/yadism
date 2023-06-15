3cc88a6660c4ef28730a3a9a83396fbf3cb00038
Radonirinaunimi <rrabeman@nikhef.nl>
2023-06-15 23:28:38 +0200

    Add generator for nEMC

---
+++ extras/data/machinery/generate/EMC.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from .utils import load, obs_template
+
+
+def dump(src_path, target):
+    """Compute SLAC observables.
+
+    Parameters
+    ----------
+    src_path : str
+        input path
+
+    Returns
+    -------
+    dict
+        observables dictionary, corresponding to the runcard
+
+    """
+    del target
+
+    obs = obs_template.copy()
+    data = load(src_path, 1, ["-", "x", "Q2", "y"])
+    esf = [dict(x=d["x"], y=d["y"], Q2=d["Q2"]) for d in data]
+
+    obs["prDIS"] = "NC"
+    obs["observables"] = {"F2_total": esf}
+    obs["ProjectileDIS"] = "electron"
+
+    obs["TargetDIS"] = "proton"
+
+    return obs
+
+
+# renaming
+new_names = {
+    "emc_c_d": "EMC90_C_D",
+    "emc_ca_d": "EMC90_Ca_D",
+    "emc_cu_d": "EMC93_Cu_D",
+    "emc_fe_d": "EMC97_Fe_D",
+    "emc_sn_d": "EMC88_Sn_D",
+}
--- extras/data/machinery/generate/__init__.py
+++ extras/data/machinery/generate/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from . import BCDMS, CHORUS, HERA, NMC, NUTEV, POS, SLAC, utils
+from . import BCDMS, CHORUS, HERA, EMC, NMC, NUTEV, POS, SLAC, utils
 
 exps = {
     getattr(m, "__name__").rsplit(".", maxsplit=1)[-1]: m
-    for m in [BCDMS, CHORUS, HERA, NMC, NUTEV, POS, SLAC]
+    for m in [BCDMS, CHORUS, HERA, EMC, NMC, NUTEV, POS, SLAC]
 }
 
 
