e8f2dcc66414b7e073c53147dac3232a5ea44609
Radonirinaunimi <rrabeman@nikhef.nl>
2023-06-15 22:57:33 +0200

    Add NMC generators

--- extras/data/machinery/generate/NMC.py
+++ extras/data/machinery/generate/NMC.py
@@ -34,7 +34,11 @@ def dump(src_path, target):
         obs["observables"] = {"XSHERANC": esf}
         obs["TargetDIS"] = "proton"
     else:
-        data = load(str(src), 0, ["x", "Q2"])
+        if len(src.stem.split("_")) == 3:
+            data =load(str(src), 1, ["-", "x", "Q2"])
+        else:
+            data = load(str(src), 0, ["x", "Q2"])
+
         esf = [dict(x=d["x"], y=1.0, Q2=d["Q2"]) for d in data]
         obs["observables"] = {"F2_total": esf}
         obs["TargetDIS"] = "proton" if "_P_" in target.parent.name else "isoscalar"
@@ -49,4 +53,16 @@ def dump(src_path, target):
 new_names = {
     "nmc_p": "NMC_NC_EM_P_SIGMARED",
     "nmc_f2df2p": ["NMC_NC_EM_P_F2", "NMC_NC_EM_D_F2"],
+    "nmc_al_c": "NMC96_Al_C",
+    "nmc_be_c": "NMC96_Be_C",
+    "nmc_c_d": "NMC95_C_D",
+    "nmc_c_li": "NMC95RE_C_Li",
+    "nmc_ca_c": "NMC96_Ca_C",
+    "nmc_ca_d": "NMC95RE_Ca_D",
+    "nmc_ca_li": "NMC95RE_Ca_Li",
+    "nmc_fe_c": "NMC96_Fe_C",
+    "nmc_he_c": "NMC96_He_C",
+    "nmc_li_d": "NMC96_Li_D",
+    "nmc_pb_c": "NMC96_Pb_C",
+    "nmc_sn_c": "NMC96_Sn_C",
 }
