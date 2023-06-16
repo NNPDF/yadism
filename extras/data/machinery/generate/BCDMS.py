1b98a03f14755539ec5a5de58079e94dd382abab
Radonirinaunimi <rrabeman@nikhef.nl>
2023-06-16 09:30:12 +0200

    Add generator for nBCDMS

--- extras/data/machinery/generate/BCDMS.py
+++ extras/data/machinery/generate/BCDMS.py
@@ -21,23 +21,30 @@ def dump(src_path, _target):
     obs = obs_template.copy()
     src = pathlib.Path(src_path)
 
-    esf = []
-    for sub in sorted(src.glob("*")):
-        text = sub.read_text()
-        with tempfile.NamedTemporaryFile(mode="w") as ntf:
-            cut_text = "\n".join(text.splitlines()[::2])
-            ntf.write(cut_text)
-            ntf.flush()
-            data = load(ntf.name, 0, ["x", "y", "Q2"])
-            esf += [dict(x=d["x"], y=d["y"], Q2=d["Q2"]) for d in data]
+    if src.stem == "bcd_d" or src.stem == "bcd_p":
+        esf = []
+        for sub in sorted(src.glob("*")):
+            text = sub.read_text()
+            with tempfile.NamedTemporaryFile(mode="w") as ntf:
+                cut_text = "\n".join(text.splitlines()[::2])
+                ntf.write(cut_text)
+                ntf.flush()
+                data = load(ntf.name, 0, ["x", "y", "Q2"])
+                esf += [dict(x=d["x"], y=d["y"], Q2=d["Q2"]) for d in data]
+    else:
+        data =load(str(src), 0, ["-", "x", "Q2", "y"])
+        esf = [dict(x=d["x"], y=d["y"], Q2=d["Q2"]) for d in data]
 
     obs["prDIS"] = "NC"
     obs["observables"] = {"F2_total": esf}
     obs["ProjectileDIS"] = "electron"
+
     if "bcd_p" in src.stem:
         obs["TargetDIS"] = "proton"
     elif "bcd_d" in src.stem:
         obs["TargetDIS"] = "isoscalar"
+    elif len(src.stem.split("_")) == 3:
+        obs["TargetDIS"] = "proton"
     else:
         raise ValueError("BCDMS unknown data")
 
@@ -48,4 +55,6 @@ def dump(src_path, _target):
 new_names = {
     "bcd_d": "BCDMS_NC_EM_D_F2",
     "bcd_p": "BCDMS_NC_EM_P_F2",
+    "bcdms_fe_d": "BCDMS85_Fe_D",
+    "bcdms_n_d": "BCDMS85_N_D",
 }
