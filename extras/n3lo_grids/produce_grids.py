092cbc7692c928f85bf257dd4d9d5536d28b0ae4
niccolo <niclaurenti@gmail.com>
2023-11-16 10:56:36 +0100

    Add new grids

--- extras/n3lo_grids/produce_grids.py
+++ extras/n3lo_grids/produce_grids.py
@@ -1,15 +1,18 @@
-import numpy as np
 import pathlib
+
+import numpy as np
 
 here = pathlib.Path(__file__).parent / "yadism_grids"
 here.mkdir(exist_ok=True)
 
+
 def produce_grids():
-    etalist = np.geomspace(1e-6, 1e6, 73, endpoint=True)
-    xilist = np.geomspace(1e-3, 1e5, 49, endpoint=True)
-    
+    etalist = np.geomspace(1e-5, 9.9e5, 73, endpoint=True)
+    xilist = np.geomspace(1e-3, 1e4, 49, endpoint=True)
+
     np.save(here / "eta.npy", etalist)
     np.save(here / "xi.npy", xilist)
+
 
 if __name__ == "__main__":
     produce_grids()
