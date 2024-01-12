6b77920c7dca04681b2b3c77064210ea2beaefae
Radonirinaunimi <rrabeman@nikhef.nl>
2024-01-12 10:56:42 +0100

    completely restructure BCDMS

--- extras/data/machinery/generate/BCDMS.py
+++ extras/data/machinery/generate/BCDMS.py
@@ -1,7 +1,7 @@
 import pathlib
 import tempfile
 
-from .utils import load, obs_template
+from .utils import load, obs_template, check_duplicate_kins
 
 
 def dump(src_path, _target):
@@ -20,15 +20,22 @@ def dump(src_path, _target):
     """
     obs = obs_template.copy()
     src = pathlib.Path(src_path)
+    print(f"**** {src.stem}")
 
     esf = []
-    text = src.read_text()
-    with tempfile.NamedTemporaryFile(mode="w") as ntf:
-        cut_text = "\n".join(text.splitlines()[::2])
-        ntf.write(cut_text)
-        ntf.flush()
-        data = load(ntf.name, 0, ["x", "y", "Q2"])
-        esf += [dict(x=d["x"], y=d["y"], Q2=d["Q2"]) for d in data]
+    if "bcd_" in src.stem:  # Proton Dataset
+        text = src.read_text()
+        with tempfile.NamedTemporaryFile(mode="w") as ntf:
+            cut_text = "\n".join(text.splitlines()[::2])
+            ntf.write(cut_text)
+            ntf.flush()
+            data = load(ntf.name, 0, ["x", "y", "Q2"])
+            esf += [dict(x=d["x"], y=d["y"], Q2=d["Q2"]) for d in data]
+    else:
+        data = load(str(src), 0, ["-", "x", "Q2", "y"])
+        esf = [dict(x=d["x"], y=d["y"], Q2=d["Q2"]) for d in data]
+    check_duplicate_kins(esf, subset=["x", "Q2", "y"])
+
     obs["prDIS"] = "NC"
     obs["observables"] = {"F2_total": esf}
     obs["ProjectileDIS"] = "electron"
@@ -50,4 +57,7 @@ new_names = {
     "bcd_p120": "BCDMS_NC_120GEV_EM_P_F2",
     "bcd_p200": "BCDMS_NC_200GEV_EM_P_F2",
     "bcd_p280": "BCDMS_NC_280GEV_EM_P_F2",
+    "bcdms_d": "BCDMS_NC_D_F2",
+    "bcdms_fe_d": ["BCDMS_85_NC_Fe_D_F2_NUM", "BCDMS_85_NC_Fe_D_F2_DEN"],
+    "bcdms_n_d": ["BCDMS_85_NC_N_D_F2_NUM", "BCDMS_85_NC_N_D_F2_DEN"],
 }
