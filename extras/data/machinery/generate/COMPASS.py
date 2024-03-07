97f88297a5e354339e909e481cfeff474e0f557d
toonhasenack <toon.hasenack@outlook.com>
2024-03-07 11:25:11 +0100

    dump to utils

--- extras/data/machinery/generate/COMPASS.py
+++ extras/data/machinery/generate/COMPASS.py
@@ -1,45 +1,7 @@
-from .utils import load, obs_template
-def dump(src_path, target):
-    '''Generate the input card for COMPASS measurements.
-
-    Parameters
-    ----------
-    src_path : str
-        input path
-
-    Returns
-    -------
-    dict
-        observables dictionary, corresponding to the runcard
-
-    '''
-    obs = obs_template.copy()
-    data = load(src_path, 0, ["x", "Q2"])
-    dict_kins = [
-        dict(x=d["x"]["mid"], y=d["y"]["mid"], Q2=d["Q2"]["mid"])
-        for d in data
-    ]
-
-
-    obs["PolarizationDIS"] = 0.0 if "_F1" in target.parent.name else 1.0
-    observable_name = "F1_total" if "_F1" in target.parent.name else "g1_total"
-    obs["observables"] = {observable_name: dict_kins}
-    if "_ep_" in str(src_path.stem) or "_mup_" in str(src_path.stem):
-        obs["TargetDIS"] = "proton"
-    elif "_en_" in str(src_path.stem) or "_mun_" in str(src_path.stem):
-        obs["TargetDIS"] = "neutron"
-    elif "_ed_" in str(src_path.stem) or "_mud_" in str(src_path.stem):
-        obs["TargetDIS"] = "isoscalar"
-
-    # Details regarding the observables
-    obs["prDIS"] = "NC"
-    obs["ProjectileDIS"] = "electron"
-
-    return obs
-
+from .utils import dump_polarized as dump
 
 # renaming
-new_names = { 
- 'compass_mud_g1': 'COMPASS_NC_25GEV_MUD_G1',
-    'compass_mup_g1': 'COMPASS_NC_17GEV_MUP_G1',
+new_names = {
+    "compass_mud_g1": "COMPASS_NC_25GEV_MUD_G1",
+    "compass_mup_g1": "COMPASS_NC_17GEV_MUP_G1",
 }
--- extras/data/machinery/generate/utils.py
+++ extras/data/machinery/generate/utils.py
@@ -65,6 +65,45 @@ def dump(exp, path, new_name):
             m.write(f"{k}={v}\n")
 
 
+def dump_polarized(src_path, target):
+    """Generate the input card for polarized measurements.
+
+    Parameters
+    ----------
+    src_path : str
+        input path
+    target: str
+        target nucleon
+
+    Returns
+    -------
+    dict
+        observables dictionary, corresponding to the runcard
+
+    """
+    obs = obs_template.copy()
+    data = load(src_path, 0, ["x", "Q2"])
+    dict_kins = [
+        dict(x=d["x"]["mid"], y=d["y"]["mid"], Q2=d["Q2"]["mid"]) for d in data
+    ]
+
+    obs["PolarizationDIS"] = 0.0 if "_F1" in target.parent.name else 1.0
+    observable_name = "F1_total" if "_F1" in target.parent.name else "g1_total"
+    obs["observables"] = {observable_name: dict_kins}
+    if "_ep_" in str(src_path.stem) or "_mup_" in str(src_path.stem):
+        obs["TargetDIS"] = "proton"
+    elif "_en_" in str(src_path.stem) or "_mun_" in str(src_path.stem):
+        obs["TargetDIS"] = "neutron"
+    elif "_ed_" in str(src_path.stem) or "_mud_" in str(src_path.stem):
+        obs["TargetDIS"] = "isoscalar"
+
+    # Details regarding the observables
+    obs["prDIS"] = "NC"
+    obs["ProjectileDIS"] = "electron"
+
+    return obs
+
+
 def metadata(path, new_name):
     metadata = metadata_template.copy()
     metadata["nnpdf_id"] = path.stem
