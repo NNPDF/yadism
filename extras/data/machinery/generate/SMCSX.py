785e4e745d41510a9329e8a77d8b7452d6179625
giacomomagni <giac.magni@gmail.com>
2024-03-07 17:00:00 +0100

    another round of metadata update

--- extras/data/machinery/generate/SMCSX.py
+++ extras/data/machinery/generate/SMCSX.py
@@ -2,6 +2,6 @@ from .utils import dump_polarized as dump
 
 # renaming
 new_names = {
-    "smcsx_mud_g1": ["SMCSX_NC_24GEV_MUD_G1", "SMCSX_NC_24GEV_MUD_F1"],
-    "smcsx_mup_g1": ["SMCSX_NC_17GEV_MUP_G1", "SMCSX_NC_17GEV_MUP_F1"],
+    "smcsx_mud_g1f1": ["SMCSX_NC_24GEV_MUD_G1", "SMCSX_NC_24GEV_MUD_F1"],
+    "smcsx_mup_g1f1": ["SMCSX_NC_17GEV_MUP_G1", "SMCSX_NC_17GEV_MUP_F1"],
 }
