742301f66009ff7a1304099c5a2ed3c789636fe4
Toon Hasenack <Toon.Hasenack@outlook.com>
2024-03-26 17:06:42 +0100

    propagate NOTFIXED namechange

--- extras/data/machinery/generate/EMC.py
+++ extras/data/machinery/generate/EMC.py
@@ -2,5 +2,5 @@ from .utils import dump_polarized as dump
 
 # renaming
 new_names = {
-    "emc_mup_g1": "EMC_NC_16GEV_MUP_G1",
+    "emc_mup_g1": "EMC_NC_NOTFIXED_MUP_G1",
 }
--- extras/data/machinery/generate/JLABEG1B.py
+++ extras/data/machinery/generate/JLABEG1B.py
@@ -2,6 +2,6 @@ from .utils import dump_polarized as dump
 
 # renaming
 new_names = {
-    "jlabeg1b_ep_g1f1": ["JLABEG1B_NC_3GEV_EP_G1", "JLABEG1B_NC_3GEV_EP_F1"],
-    "jlabeg1b_ed_g1f1": ["JLABEG1B_NC_4GEV_ED_G1", "JLABEG1B_NC_4GEV_ED_F1"],
+    "jlabeg1b_ep_g1f1": ["JLABEG1B_NC_NOTFIXED_EP_G1", "JLABEG1B_NC_NOTFIXED_EP_F1"],
+    "jlabeg1b_ed_g1f1": ["JLABEG1B_NC_NOTFIXED_ED_G1", "JLABEG1B_NC_NOTFIXED_ED_F1"],
 }
