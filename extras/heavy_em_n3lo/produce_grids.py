e9b1fc28864004ef29d4846490bf55ee950deaa0
Roy Stegeman <roystegeman@live.nl>
2024-11-01 13:46:43 +0000

    extend the n3lo heavy grids up to xi=1e9

--- extras/heavy_em_n3lo/produce_grids.py
+++ extras/heavy_em_n3lo/produce_grids.py
@@ -8,7 +8,7 @@ here.mkdir(exist_ok=True)
 
 def produce_grids():
     etalist = np.geomspace(1e-5, 9.9e5, 73, endpoint=True)
-    xilist = np.geomspace(1e-3, 1e4, 49, endpoint=True)
+    xilist = np.geomspace(1e-3, 1e9, 84, endpoint=True)
 
     np.save(here / "eta.npy", etalist)
     np.save(here / "xi.npy", xilist)
