8980754e8326c77b95ac7850658687f0238f062b
Jelle Koorn <jkoorn@nikhef.nl>
2026-08-12 15:57:51 +0200

    remove else

--- src/yadism/coefficient_functions/heavy/kernels.py
+++ src/yadism/coefficient_functions/heavy/kernels.py
@@ -78,7 +78,6 @@ def generate(esf, nf, ihq):
         if "FL" in kind:
             n3lo_g_variation = esf.info.theory["n3lo_cf_variation"][2]
             n3lo_q_variation = esf.info.theory["n3lo_cf_variation"][3]
-        else
         gVV = kernels.Kernel(
             weights["gVV"],
             pcs.GluonVV(esf, nf, m2hq=m2hq, n3lo_cf_variation=n3lo_g_variation),
